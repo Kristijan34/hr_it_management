e6873df879169d5f8619c9de4d5d577791458c39
KristijanL <52075405+Kristijan34@users.noreply.github.com>
2024-03-31 21:03:26 +0200

    Create modules

---
+++ custom/Extension/application/Ext/Include/hr.php
@@ -0,0 +1,16 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['hr_Employee_absences'] = 'hr_Employee_absences';
+$beanFiles['hr_Employee_absences'] = 'modules/hr_Employee_absences/hr_Employee_absences.php';
+$moduleList[] = 'hr_Employee_absences';
+$beanList['hr_Position_management'] = 'hr_Position_management';
+$beanFiles['hr_Position_management'] = 'modules/hr_Position_management/hr_Position_management.php';
+$moduleList[] = 'hr_Position_management';
+$beanList['hr_Regions'] = 'hr_Regions';
+$beanFiles['hr_Regions'] = 'modules/hr_Regions/hr_Regions.php';
+$moduleList[] = 'hr_Regions';
+$beanList['hr_Stores'] = 'hr_Stores';
+$beanFiles['hr_Stores'] = 'modules/hr_Stores/hr_Stores.php';
+$moduleList[] = 'hr_Stores';
+
+?>
